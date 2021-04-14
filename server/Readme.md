ab8535210d6491f88644f0b8570eb362d5dafaef
unknown <kittitat.nm@gmail.com>
2021-04-14 22:18:24 +0700

    Request and Response

---
+++ server/Readme.md
@@ -0,0 +1,6 @@
+# client and Server
+
+
+
+### Credit. 
+https://www.youtube.com/watch?v=-HPZ1leCV8k&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=3&ab_channel=TheNetNinja
--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
 const http = require('http')
 
 const server = http.createServer((req, res) => {
-    console.log('request made')
-    
+    console.log(req.url, req.method)
+    // set header content type
+    res.setHeader('Content-Type', 'text/html')
+    res.write('<h1>Hello world</h1>')
+    res.end()
 })
 
 server.listen(3000, 'localhost', () => {
