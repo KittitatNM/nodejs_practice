9dfc9ae232a408f740bdd8dc268b32056ee3c038
unknown <kittitat.nm@gmail.com>
2021-04-14 21:41:39 +0700

    Learn File System

--- files.js
+++ files.js
@@ -1,1 +1,47 @@
+// https://www.youtube.com/watch?v=OIBIXYLJjsI&list=PL4cUxeGkcC9jsz4LDYc6kv3ymONOKxwBU&index=2&ab_channel=TheNetNinja
+
 const fs = require('fs')
+const { lookupService } = require('node:dns')
+
+if (!fs.existsSync('./docs')) {
+    fs.mkdir('./docs', (err) => {
+        if (err) {
+            console.log(err)
+        }
+        else {
+            console.log('Create Directory Success')
+        }
+    })
+}
+// else {
+//     fs.rmdir('./docs', {
+//         recursive: true,
+//     }, (err) => {
+//         if (err) {
+//             console.log(err)
+//         }
+//         else {
+//             console.log('Remove Directory Success')
+//         }
+//     })
+// }
+
+//  remove recursive directory
+
+// fs.writeFile('./docs/blog.txt', 'HelloWorld', (err) => {
+//     if (err) {
+//         console.log(err)
+//     }
+//     else {
+//         console.log('Create File Success')
+//     }
+// })
+
+fs.readFile('./docs/blog.txt','utf-8', (err, data) => {
+    if (err) {
+        console.log(err)
+    }
+    else {
+        console.log(data)
+    }
+})
---
+++ streams.js
@@ -0,0 +1,11 @@
+const fs = require('fs')
+
+const readStream = fs.createReadStream('./docs/blog.txt', { encoding: 'utf-8' })
+const writeSteam = fs.createWriteStream('./docs/blog1.txt')
+
+// readStream.on('data', (chunk) => {
+//     console.log(chunk)
+//     writeSteam.write(chunk)
+// })
+
+readStream.pipe(writeSteam)
